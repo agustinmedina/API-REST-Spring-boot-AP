30ea12357d54a3dc4c677058e19c8bcee4cd6574
agustinmedina1 <agustin_trompiz1@hotmail.com>
2022-04-27 16:06:49 -0300

    first commit

---
+++ src/main/java/com/example/holamundo/IPersonaService.java
@@ -0,0 +1,12 @@
+package com.example.holamundo;
+import java.util.List;
+
+
+public interface IPersonaService {
+    public List<Persona> verPersonas();
+    public void crearPersona (Persona per);
+    public void borrarPersona(Long id);
+    public Persona buscarPersona(Long id);
+    //en el ultimo caso devuelve persona por eso no va void
+    
+}
---
+++ src/main/java/com/example/holamundo/Persona.java
@@ -0,0 +1,59 @@
+package com.example.holamundo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+    private Long id;
+    private String nombre;
+    private String apellido;
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+
+    public String getApellido() {
+        return apellido;
+    }
+
+
+
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Persona(){
+    
+    }
+
+    public Persona(Long id, String nombre, String apellido) {
+        this.id = id;
+        this.setNombre(nombre);
+        this.setApellido(apellido);
+    }
+    
+
+
+}
