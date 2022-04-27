40cb7e53195b70ae1a120b11ae4eb0072b57609a
agustinmedina1 <agustin_trompiz1@hotmail.com>
2022-04-27 19:00:27 -0300

    probando el id

--- src/main/java/com/example/holamundo/Persona.java
+++ src/main/java/com/example/holamundo/Persona.java
@@ -20,11 +20,18 @@ public class Persona {
 
     
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId (Long id) {
+        this.id = id;
+    }
+
+
     public String getNombre() {
         return nombre;
     }
-
 
 
 
@@ -51,10 +58,12 @@ public class Persona {
     }
 
     public Persona(Long id, String nombre, String apellido) {
-        this.id = id;
+        this.setId(id);
         this.setNombre(nombre);
         this.setApellido(apellido);
     }
+
+
     
 
 
